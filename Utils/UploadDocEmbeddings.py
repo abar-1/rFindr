5381301b9e4d4efcc5b4207b0c0d5ffbd5786caf
SohumK <109327670+S-K-23@users.noreply.github.com>
2025-10-21 00:34:06 -0400

    Edited upload logic

--- Utils/UploadDocEmbeddings.py
+++ Utils/UploadDocEmbeddings.py
@@ -1,5 +1,6 @@
 from ragUtils import DocumentChunker, GenerateEmbeddings, WebScrapper
 import supabase
+import json
 from supabase import create_client, Client
 
 VDB_API_URL = ""
@@ -18,11 +19,20 @@ def chunkPlainText(plainText: str) -> list[str]:
 def chunkDocuments(docPaths: list[str]) -> list[str]:
     return docChunker.chunk_documents_paragraphs(docPaths)
 
-def uploadEmbeddingsToVDB(embeddings: list[list[float]]):
+def uploadEmbeddingToVDB(embedding: list[float], chunk: str = ""):
     client = create_client(VDB_API_URL, VDB_ANON_KEY)
+    JSON_data = [{"chunk": chunk, "embedding": embedding}]
     try:
-        client.storage.from_('embeddings').upload('embeddings.json', embeddings)
+        JSON_data = json.dumps(JSON_data, ensure_ascii=False, indent=2)
+
+        client.storage.from_('embeddings').upload(
+            path = 'embeddings.json', 
+            file = JSON_data.encode('utf-8'),
+            file_options={"content-type": "application/json"}            
+            )
+        
         print("Successfully uploaded embeddings to VDB.")
+    
     except Exception as e:
         print(f"Error uploading embeddings: {e}")
     
@@ -36,7 +46,8 @@ chunks = chunkDocuments(docPaths)
 #chunks = chunkPlainText(plainText)
 
 embeddings = embedGenerator.generate_Embeddings(chunks)
-uploadEmbeddingsToVDB(embeddings)
+for chunk, embedding in zip(chunks, embeddings):
+    uploadEmbeddingToVDB(embedding, chunk)
 
 
  
